db2c49de48a7d70b8e5143e3ff62d2f6ac6cfc4e
tejasmore143 <cs.tejasmore64@gmail.com>
2026-02-15 14:17:03 +0530

    Fix indentation in prediction button logic

--- app.py
+++ app.py
@@ -35,8 +35,8 @@ df = pd.DataFrame({
 
 
 if st.button("Predict"):
-  for col in encoder:
-    df[col] = encoder[col].transform(df[col])
+    for col in encoder:
+        df[col] = encoder[col].transform(df[col])
 
     prediction = model.predict(df)
     st.success(f"Predicted Salary : {prediction[0]:,.2f}")
